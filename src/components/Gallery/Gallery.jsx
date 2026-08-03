1e37f6dac0e6f7f6a7bf77d94498cd17e0f05e1c
Shriram Sharma <shriram1157@gmail.com>
2026-08-03 16:35:39 +0530

    Fix gallery image extension case

--- src/components/Gallery/Gallery.jsx
+++ src/components/Gallery/Gallery.jsx
@@ -1,11 +1,11 @@
 import "./Gallery.css";
 
-import img1 from "../../assets/images/gallery/1.jpg";
-import img2 from "../../assets/images/gallery/2.jpg";
-import img3 from "../../assets/images/gallery/3.jpg";
-import img4 from "../../assets/images/gallery/4.jpg";
-import img5 from "../../assets/images/gallery/5.jpg";
-import img6 from "../../assets/images/gallery/6.jpg";
+import img1 from "../../assets/images/gallery/1.JPG";
+import img2 from "../../assets/images/gallery/2.JPG";
+import img3 from "../../assets/images/gallery/3.JPG";
+import img4 from "../../assets/images/gallery/4.JPG";
+import img5 from "../../assets/images/gallery/5.JPG";
+import img6 from "../../assets/images/gallery/6.JPG";
 
 function Gallery() {
 
