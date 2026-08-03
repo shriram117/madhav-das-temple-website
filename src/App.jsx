c74cc7cb29648c320d2642e5b10cab077cac59fc
Shriram Sharma <shriram1157@gmail.com>
2026-08-03 16:23:17 +0530

    Rename navbar.jsx to Navbar.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import TopBar from "./components/TopBar/TopBar";
-import Navbar from "./components/Navbar/navbar";
+import Navbar from "./components/Navbar/Navbar";
 import Hero from "./components/Hero/Hero";
 import AboutTemple from "./components/AboutTemple/AboutTemple";
 import DailyAarti from "./components/DailyAarti/DailyAarti";
